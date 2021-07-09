bea20d0e134d12fa13039bc8f4b99eda5b40ed34
Santiago Cortes <santi.lamarin98@gmail.com>
2021-07-09 11:52:06 -0500

    ejemplos hoisting

---
+++ src/hoisting/index.js
@@ -0,0 +1,15 @@
+a = 2;
+var a;
+console.log(a);// resultado de 2
+//hoisting eleva las declaraciones de variables
+
+console.log(a);//undefined
+var a = 2;
+
+//===================================================
+// se ajusta o adaptan en el navegador
+nameOfDog('Tommy');
+function nameOfDog(name){
+    console.log(name)
+}
+
